3bff698480699e5cd8cf619c4effae1ee49c3303
Emmanuel Jaep <emmanuel.jaep@epfl.ch>
2020-02-12 14:51:59 +0100

    update the way to create the jupyter notebook container in order to pre-install python requirements in the container.

--- scripts/start_jupyter.sh
+++ scripts/start_jupyter.sh
@@ -1,8 +1,10 @@
 #!/usr/bin/env bash
 
+docker build -t epfl_sti/budget_simulator_jupyter_notebook:latest ./scripts
+
 docker run --rm \
             -p 8888:8888 \
             -e JUPYTER_ENABLE_LAB=yes \
             -v "$PWD":/home/jovyan/work \
-            --name jupyter \
-            jupyter/datascience-notebook
+            --name budget_simulator_jupyter_notebook \
+            epfl_sti/budget_simulator_jupyter_notebook
