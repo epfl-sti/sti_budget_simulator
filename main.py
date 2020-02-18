a3bb04f983246a53ef8b69600e48aa22d6aeff96
Emmanuel Jaep <emmanuel.jaep@epfl.ch>
2020-02-18 09:51:02 +0100

    add simple runner

---
+++ main.py
@@ -0,0 +1,39 @@
+import pandas as pd
+
+from rules import budget
+from settings import main as settings
+
+
+def __get_parameters():
+    params = pd.read_excel(settings.PARAMETERS_FILE)
+    return params
+
+
+def __dump_output(df):
+    df.to_csv(settings.OUTPUT_FILE, index=False)
+
+
+def main():
+    params = __get_parameters()
+    simulation_start = settings.START_DATE
+    simulation_end = settings.END_DATE
+
+    return_value = pd.DataFrame()
+
+    for index, row in params.iterrows():
+        run_params = {}
+        run_params['initial_budget'] = row['initial budget']
+        run_params['start_date'] = simulation_start
+        run_params['end_date'] = simulation_end
+        run_params['academic_rank'] = row['academic rank']
+        run_params['in_rank_since'] = row['in rank since']
+        run_params['CF'] = row['CF']
+        milestones, current_df = budget.main(run_params)
+
+        return_value = pd.concat([return_value, current_df], ignore_index=True)
+
+    __dump_output(return_value)
+
+
+if __name__ == "__main__":
+    main()
---
+++ settings/main.py
@@ -0,0 +1,6 @@
+import datetime
+
+PARAMETERS_FILE = 'parameters.xlsx'
+START_DATE = datetime.datetime(2019,1,1)
+END_DATE = datetime.datetime(2029,1,1)
+OUTPUT_FILE = 'out.csv'
