d28a525238d4a59e14c267a581ba1a50bce06c1d
Emmanuel Jaep <emmanuel.jaep@epfl.ch>
2020-02-27 13:52:14 +0100

    add module to read excel files provided by A. Barras

---
+++ settings/datagenerator.py
@@ -0,0 +1,4 @@
+EXCEL_FILE_PATH = "./src/Data_faculty_list_20200221.xlsx"
+SAP_RH_EXPORT_SHEETNAME = 'Extraction SAP'
+FACULTY_MEMBERS_SHEETNAME = 'Faculty Members'
+CACHE_FILE_PATH = './cache/faculty_members.pkl'
