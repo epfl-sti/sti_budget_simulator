566b31f9217c3fc96c1b6884e1b12c3c090d52e8
Emmanuel Jaep <emmanuel.jaep@epfl.ch>
2020-05-06 14:01:54 +0200

    refactor

--- main.py
+++ main.py
@@ -10,64 +10,13 @@ from settings import main as settings
 logger = logging.getLogger(__name__)
 
 
-def __get_parameters():
-    logger.info("getting parameters from file")
-    logger.debug("Parameters file: {}".format(settings.PARAMETERS_FILE))
-    params = pd.read_excel(settings.PARAMETERS_FILE)
-
-    logger.info("done")
-    return params
-
-
 def __dump_output(df):
     logger.info("Dumping output to file")
     logger.debug("file path: {}".format(settings.OUTPUT_FILE))
     df.to_csv(settings.OUTPUT_FILE, index=False)
     logger.info("done")
 
-
-def __dump_milestones(milestones):
-    """
-    expected content of dictionaries
-    'DoB'
-    'patt_promotion'
-    'first_bump_budget_increase_date'
-    'pa_promotion'
-    'po_promotion'
-    'po_step1
-    'po_step2'
-    'po_step3'
-    'po_full'
-    'retirement'
-    """
-    logger.info("dumping milestones to disk")
-    logger.debug("output file path: {}".format(settings.MILESTONES_OUTPUT_FILE))
-    logger.debug("Number of CFs: {}".format(len(milestones)))
-
-    columns = [
-        "CF",
-        "DoB",
-        "patt_promotion",
-        "pa_promotion",
-        "po_promotion",
-        "retirement",
-    ]
-    df = pd.DataFrame(columns=columns)
-    for key, value in milestones.items():
-        CF = key
-        DoB = value["DoB"]
-        patt_promotion = value["patt_promotion"]
-        pa_promotion = value["pa_promotion"]
-        po_promotion = value["po_promotion"]
-        retirement = value["retirement"]
-        df.loc[len(df)] = [
-            CF,
-            DoB,
-            patt_promotion,
-            pa_promotion,
-            po_promotion,
-            retirement,
-        ]
+    df = pd.DataFrame(milestones)
     df.to_csv(settings.MILESTONES_OUTPUT_FILE, index=False)
 
 
@@ -86,53 +35,29 @@ def __final_cleanup(df):
     return df
 
 
-def main():
+def main(params):
     logger.info("Started running the simulation")
+
+    # boilerplate
     return_value = pd.DataFrame()
-    all_milestones = {}
 
-    # Budget Rules
-    # __generate_parameters()
+    # Lab budget rule
+    logger.info("Running the lab budgets rules")
+    run_params = {
+        "start_date": params["simulation_start"],
+        "end_date": params["simulation_end"],
+    }
+    current_df, milestones = budget.main(run_params)
+    return_value = pd.concat([return_value, current_df], ignore_index=True)
 
-    params = __get_parameters()
-    simulation_start = settings.START_DATE
-    simulation_end = settings.END_DATE
-
-    logger.info("Started running the budget rules")
-    for index, row in params.iterrows():
-        run_params = {}
-        run_params["start_date"] = simulation_start
-        run_params["end_date"] = simulation_end
-        run_params["CF"] = row["CF"]
-        run_params["DOB"] = row["DOB"]
-        if not pd.isnull(row["PATT promotion"]):
-            run_params["PATT promotion"] = row["PATT promotion"]
-        if not pd.isnull(row["PA promotion"]):
-            run_params["PA promotion"] = row["PA promotion"]
-        if not pd.isnull(row["PO promotion"]):
-            run_params["PO promotion"] = row["PO promotion"]
-        if not pd.isnull(row["retirement"]):
-            run_params["retirement"] = row["retirement"]
-
-        if not math.isnan(row["PATT yearly budget"]):
-            run_params["PATT_yearly_budget"] = row["PATT yearly budget"]
-        if not math.isnan(row["PA yearly budget"]):
-            run_params["PA_yearly_budget"] = row["PA yearly budget"]
-        if not math.isnan(row["PO yearly budget"]):
-            run_params["PO_yearly_budget"] = row["PO yearly budget"]
-        milestones, current_df = budget.main(run_params)
-
-        return_value = pd.concat([return_value, current_df], ignore_index=True)
-        all_milestones[row["CF"]] = milestones
-
-    __dump_milestones(all_milestones)
+    __dump_milestones(milestones)
     logger.info("done")
 
     # Fixed budgets
     logger.info("Running the fixed budgets rules")
     run_params = {
-        "start_date": simulation_start,
-        "end_date": simulation_end,
+        "start_date": params["simulation_start"],
+        "end_date": params["simulation_end"],
         "ledger": return_value,
     }
     df_fixed_budgets = fixed_budget.main(run_params)
@@ -141,14 +66,20 @@ def main():
 
     # Adjustments
     logger.info("Started running the adjustments rules")
-    run_params = {"start_date": simulation_start, "end_date": simulation_end}
+    run_params = {
+        "start_date": params["simulation_start"],
+        "end_date": params["simulation_end"],
+    }
     df_adjusments = adjustments.main(run_params)
     return_value = pd.concat([return_value, df_adjusments], ignore_index=True)
     logger.info("done")
 
     # Yearly budgets
     logger.info("Starting running the yearly budget rules")
-    run_params = {"start_date": simulation_start, "end_date": simulation_end}
+    run_params = {
+        "start_date": params["simulation_start"],
+        "end_date": params["simulation_end"],
+    }
     df_yearly_budgets = yearly_budget.main(run_params)
     return_value = pd.concat([return_value, df_yearly_budgets], ignore_index=True)
     logger.info("done")
@@ -160,5 +91,9 @@ def main():
 
 if __name__ == "__main__":
     logging.basicConfig(level=logging.INFO)
+    params = {
+        "simulation_start": settings.START_DATE,
+        "simulation_end": settings.END_DATE,
+    }
 
-    main()
+    main(params)
--- rules/budget.py
+++ rules/budget.py
@@ -1,6 +1,7 @@
 import calendar
 import datetime
 import logging
+import math
 import os
 import sys
 from statistics import mean
@@ -9,12 +10,13 @@ import pandas as pd
 from dateutil import rrule
 
 
-project_folder = os.path.realpath(os.path.join(os.path.dirname(__file__), '..'))
+project_folder = os.path.realpath(os.path.join(os.path.dirname(__file__), ".."))
 os.chdir(project_folder)
 sys.path.insert(0, project_folder)
 from settings import budget as settings
 
 logger = logging.getLogger(__name__)
+
 
 class __prof(object):
     def __init__(self):
@@ -26,11 +28,70 @@ class __prof(object):
         self.PA_promotion = None
         self.PO_promotion = None
         self.PATT_budget = settings.PATT_YEARLY_BUDGET / 12
-        self.PA_budget = ((settings.PATT_YEARLY_BUDGET + settings.PO_YEARLY_BUDGET) / 2) / 12
+        self.PA_budget = (
+            (settings.PATT_YEARLY_BUDGET + settings.PO_YEARLY_BUDGET) / 2
+        ) / 12
         self.PO_budget = settings.PO_YEARLY_BUDGET / 12
 
 
+def __get_parameters():
+    logger.info("getting parameters from file")
+    logger.debug("Parameters file: {}".format(settings.PARAMETERS_FILE_PATH))
+    params = pd.read_excel(settings.PARAMETERS_FILE_PATH)
+
+    logger.info("done")
+    return params
+
+
 def main(params):
+    return_value = pd.DataFrame()
+    milestones = []
+
+    CF_parameters = __get_parameters()
+    all_milestones = {}
+
+    simulation_start = params["start_date"]
+    simulation_end = params["end_date"]
+
+    logger.info("Started running the budget rules")
+    for index, row in CF_parameters.iterrows():
+
+        # Build the parameters used for this particular CF
+        run_params = {}
+        run_params["start_date"] = simulation_start
+        run_params["end_date"] = simulation_end
+        run_params["CF"] = row["CF"]
+        run_params["DOB"] = row["DOB"]
+        if not pd.isnull(row["PATT promotion"]):
+            run_params["PATT promotion"] = row["PATT promotion"]
+        if not pd.isnull(row["PA promotion"]):
+            run_params["PA promotion"] = row["PA promotion"]
+        if not pd.isnull(row["PO promotion"]):
+            run_params["PO promotion"] = row["PO promotion"]
+        if not pd.isnull(row["retirement"]):
+            run_params["retirement"] = row["retirement"]
+
+        if not math.isnan(row["PATT yearly budget"]):
+            run_params["PATT_yearly_budget"] = row["PATT yearly budget"]
+        if not math.isnan(row["PA yearly budget"]):
+            run_params["PA_yearly_budget"] = row["PA yearly budget"]
+        if not math.isnan(row["PO yearly budget"]):
+            run_params["PO_yearly_budget"] = row["PO yearly budget"]
+
+        # Calculate the ledger for that CF
+        current_milestones, current_df = calculate_ledger_for_CF(run_params)
+
+        # quickly add the CF to the milestone so we don't loose it
+        current_milestones["CF"] = row["CF"]
+
+        return_value = pd.concat([return_value, current_df], ignore_index=True)
+        milestones.append(current_milestones)
+
+    logger.info("done")
+    return return_value, milestones
+
+
+def calculate_ledger_for_CF(params):
     """
     Calculates the expenses made over a period of time according to the regular budget rules
 
@@ -43,51 +104,78 @@ def main(params):
 
     # TODO: check the parameters to make sure we have all the information we will be using
 
-    logger.info("Starting budget simulation for CF {}".format(params['CF']))
+    logger.info("Starting budget simulation for CF {}".format(params["CF"]))
 
     prof = __prof()
-    prof.CF = params['CF']
-    prof.DoB = params.get('DOB', None)
-    prof.retirementDate = params.get('retirement', None)
+    prof.CF = params["CF"]
+    prof.DoB = params.get("DOB", None)
+    prof.retirementDate = params.get("retirement", None)
 
     if prof.DoB is not None and prof.retirementDate is None:
         # The retirement date should be at the end of the year of the 'real' retirement date
         prof.retirementDate = prof.DoB + pd.offsets.DateOffset(years=65)
-        last_day_of_month = calendar.monthrange(prof.retirementDate.year, prof.retirementDate.month)[1]
-        prof.retirementDate = datetime.datetime(prof.retirementDate.year, prof.retirementDate.month, last_day_of_month)
+        last_day_of_month = calendar.monthrange(
+            prof.retirementDate.year, prof.retirementDate.month
+        )[1]
+        prof.retirementDate = datetime.datetime(
+            prof.retirementDate.year, prof.retirementDate.month, last_day_of_month
+        )
 
     if prof.DoB is None and prof.retirementDate is not None:
         prof.DoB = prof.retirementDate - pd.offsets.DateOffset(years=65)
 
-    prof.PATT_promotion = params.get('PATT promotion', None)
-    prof.PA_promotion = params.get('PA promotion', None)
-    prof.PO_promotion = params.get('PO promotion', None)
+    prof.PATT_promotion = params.get("PATT promotion", None)
+    prof.PA_promotion = params.get("PA promotion", None)
+    prof.PO_promotion = params.get("PO promotion", None)
 
     if prof.PATT_promotion is None:
         if prof.PA_promotion is not None:
-            prof.PATT_promotion = prof.PA_promotion - pd.offsets.DateOffset(months=settings.PATT_TO_PA_PERIOD)
+            prof.PATT_promotion = prof.PA_promotion - pd.offsets.DateOffset(
+                months=settings.PATT_TO_PA_PERIOD
+            )
         elif prof.PO_promotion is not None:
-            prof.PATT_promotion = prof.PO_promotion - pd.offsets.DateOffset(months=settings.PATT_TO_PA_PERIOD) - pd.offsets.DateOffset(months=settings.PA_TO_PO_PERIOD)
+            prof.PATT_promotion = (
+                prof.PO_promotion
+                - pd.offsets.DateOffset(months=settings.PATT_TO_PA_PERIOD)
+                - pd.offsets.DateOffset(months=settings.PA_TO_PO_PERIOD)
+            )
 
     if prof.PA_promotion is None:
         if prof.PATT_promotion is not None:
-            prof.PA_promotion = prof.PATT_promotion + pd.offsets.DateOffset(months=settings.PATT_TO_PA_PERIOD)
+            prof.PA_promotion = prof.PATT_promotion + pd.offsets.DateOffset(
+                months=settings.PATT_TO_PA_PERIOD
+            )
         elif prof.PO_promotion is not None:
-            prof.PA_promotion = prof.PO_promotion - pd.offsets.DateOffset(months=settings.PA_TO_PO_PERIOD)
+            prof.PA_promotion = prof.PO_promotion - pd.offsets.DateOffset(
+                months=settings.PA_TO_PO_PERIOD
+            )
 
     if prof.PO_promotion is None:
         # We have decided to calculate from the PA promotion date instead of the PATT promotion date because the PA promotion is "closer" that PATT (hence we have a better chance to know it)
         if prof.PA_promotion is not None:
-            prof.PO_promotion = prof.PA_promotion + pd.offsets.DateOffset(months=settings.PA_TO_PO_PERIOD)
+            prof.PO_promotion = prof.PA_promotion + pd.offsets.DateOffset(
+                months=settings.PA_TO_PO_PERIOD
+            )
         elif prof.PATT_promotion is not None:
-            prof.PO_promotion = prof.PATT_promotion + pd.offsets.DateOffset(months=settings.PATT_TO_PA_PERIOD) + pd.offsets.DateOffset(months=settings.PA_TO_PO_PERIOD)
+            prof.PO_promotion = (
+                prof.PATT_promotion
+                + pd.offsets.DateOffset(months=settings.PATT_TO_PA_PERIOD)
+                + pd.offsets.DateOffset(months=settings.PA_TO_PO_PERIOD)
+            )
 
-    prof.PATT_budget = params.get('PATT_yearly_budget' , settings.PATT_YEARLY_BUDGET) / 12
-    prof.PO_budget = params.get('PO_yearly_budget', settings.PO_YEARLY_BUDGET) / 12
-    prof.PA_budget = params.get('PA_yearly_budget', mean([prof.PATT_budget, prof.PO_budget])*12) / 12
+    prof.PATT_budget = (
+        params.get("PATT_yearly_budget", settings.PATT_YEARLY_BUDGET) / 12
+    )
+    prof.PO_budget = params.get("PO_yearly_budget", settings.PO_YEARLY_BUDGET) / 12
+    prof.PA_budget = (
+        params.get("PA_yearly_budget", mean([prof.PATT_budget, prof.PO_budget]) * 12)
+        / 12
+    )
 
     # calculate the date of the first bump in budget
-    first_bump_budget_increase_date = prof.PATT_promotion+pd.offsets.DateOffset(months=settings.FIRST_STEP_BUDGET_PERIOD)
+    first_bump_budget_increase_date = prof.PATT_promotion + pd.offsets.DateOffset(
+        months=settings.FIRST_STEP_BUDGET_PERIOD
+    )
 
     # calculate the beginning of the second year as PO
     po_step1 = prof.PO_promotion + pd.offsets.DateOffset(years=1)
@@ -102,16 +190,16 @@ def main(params):
     po_full = po_step3 + pd.offsets.DateOffset(years=1)
 
     milestones = {
-        'DoB': prof.DoB,
-        'patt_promotion': prof.PATT_promotion,
-        'first_bump_budget_increase_date': first_bump_budget_increase_date,
-        'pa_promotion': prof.PA_promotion,
-        'po_promotion': prof.PO_promotion,
-        'po_step1': po_step1,
-        'po_step2': po_step2,
-        'po_step3': po_step3,
-        'po_full': po_full,
-        'retirement': prof.retirementDate
+        "DoB": prof.DoB,
+        "patt_promotion": prof.PATT_promotion,
+        "first_bump_budget_increase_date": first_bump_budget_increase_date,
+        "pa_promotion": prof.PA_promotion,
+        "po_promotion": prof.PO_promotion,
+        "po_step1": po_step1,
+        "po_step2": po_step2,
+        "po_step3": po_step3,
+        "po_full": po_full,
+        "retirement": prof.retirementDate,
     }
 
     # Now that we have the various milstones, we can build a list of periods with the required information.
@@ -148,7 +236,9 @@ def main(params):
     # Period 4 is the first year after the promotion as PO
     p4_from = prof.PO_promotion
     p4_to = po_step1
-    pa_to_po_monthly_budget_increase = (prof.PO_budget - prof.PA_budget) / settings.NUMBER_OF_YEARS_TO_REACH_PO_BUDGET
+    pa_to_po_monthly_budget_increase = (
+        prof.PO_budget - prof.PA_budget
+    ) / settings.NUMBER_OF_YEARS_TO_REACH_PO_BUDGET
     p4_budget = p3_budget + pa_to_po_monthly_budget_increase
     p4_note = "1st year after the promotion as PO"
     periods.append((p4_from, p4_to, p4_budget, p4_note))
@@ -184,10 +274,10 @@ def main(params):
     periods.append((p8_from, p8_to, p8_budget, p8_note))
 
     # Now that everything is in place, we can start the simulation
-    start_date = params['start_date']
-    end_date = params['end_date']
+    start_date = params["start_date"]
+    end_date = params["end_date"]
 
-    return_value = pd.DataFrame(columns=['CF', 'date', 'budget', 'rule', 'note'])
+    return_value = pd.DataFrame(columns=["CF", "date", "budget", "rule", "note"])
 
     simulation_period = pd.date_range(start=start_date, end=end_date, freq="M")
     for current_month in simulation_period:
@@ -200,15 +290,21 @@ def main(params):
                 current_note = period[3]
                 break
 
-        return_value = return_value.append({'CF': params['CF'], 'date': current_month, 'budget': current_budget, 'rule': 'budget', 'note': current_note}, ignore_index=True)
+        return_value = return_value.append(
+            {
+                "CF": params["CF"],
+                "date": current_month,
+                "budget": current_budget,
+                "rule": "budget",
+                "note": current_note,
+            },
+            ignore_index=True,
+        )
     return milestones, return_value
 
 
 if __name__ == "__main__":
     parameters = {}
-    parameters['start_date'] = datetime.datetime(2019, 1, 1)
-    parameters['end_date'] = datetime.datetime(2029, 1, 1)
-    parameters['academic_rank'] = 'PA'
-    parameters['in_rank_since'] = datetime.datetime(2018, 1, 1)
-    parameters['CF'] = '1234'
+    parameters["start_date"] = datetime.datetime(2019, 1, 1)
+    parameters["end_date"] = datetime.datetime(2029, 1, 1)
     main(parameters)
